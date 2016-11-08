b4800766e845a1680f4b1f00550c2be064c63317
pc003 <sz_022@outlook.com>
2016-11-08 10:43:42 +0800

    test

--- src/ExpressGateway.php
+++ src/ExpressGateway.php
@@ -35,51 +35,147 @@ class ExpressGateway extends AbstractGateway
     }
 
 
-    public function setReturnUrl($value)
+    public function setAmount($value)
     {
-        return $this->setParameter('returnUrl', $value);
+        return $this->setParameter('amount', $value);
     }
 
 
-    public function getReturnUrl()
+    public function getAmount()
     {
-        return $this->getParameter('returnUrl');
+        return $this->getParameter('amount');
     }
 
 
-    public function setNotifyUrl($value)
+    public function setOrderRef($value)
     {
-        return $this->setParameter('notifyUrl', $value);
+        return $this->setParameter('orderRef', $value);
     }
 
 
-    public function getNotifyUrl()
+    public function getOrderRef()
     {
-        return $this->getParameter('notifyUrl');
+        return $this->getParameter('orderRef');
+    }
+
+
+    public function setRedirect($value)
+    {
+        return $this->setParameter('redirect', $value);
+    }
+
+
+    public function getRedirect()
+    {
+        return $this->getParameter('redirect');
+    }
+
+
+    public function setSuccessUrl($value)
+    {
+        return $this->setParameter('successUrl', $value);
+    }
+
+
+    public function getSuccessUrl()
+    {
+        return $this->getParameter('successUrl');
+    }
+
+
+    public function setFailUrl($value)
+    {
+        return $this->setParameter('failUrl', $value);
+    }
+
+
+    public function getFailUrl()
+    {
+        return $this->getParameter('failUrl');
+    }
+
+
+    public function setCancelUrl($value)
+    {
+        return $this->setParameter('cancelUrl', $value);
+    }
+
+
+    public function getCancelUrl()
+    {
+        return $this->getParameter('cancelUrl');
     }
 
 
     public function setMerId($value)
     {
-        return $this->setParameter('merId', $value);
+        return $this->setParameter('merchantId', $value);
     }
 
 
     public function getMerId()
     {
-        return $this->getParameter('merId');
+        return $this->getParameter('merchantId');
     }
 
 
-    public function setCurrencyCode($value)
+    public function setMpsMode($value)
     {
-        return $this->setParameter('currencyCode', $value);
+        return $this->setParameter('mpsMode', $value);
     }
 
 
-    public function getCurrencyCode()
+    public function getMpsMode()
     {
-        return $this->getParameter('currencyCode');
+        return $this->getParameter('mpsMode');
+    }
+
+
+    public function setCurrCode($value)
+    {
+        return $this->setParameter('currCode', $value);
+    }
+
+
+    public function getCurrCode()
+    {
+        return $this->getParameter('currCode');
+    }
+
+
+    public function setPayType($value)
+    {
+        return $this->setParameter('payType', $value);
+    }
+
+
+    public function getPayType()
+    {
+        return $this->getParameter('payType');
+    }
+
+
+    public function setLanguage($value)
+    {
+        return $this->setParameter('lang', $value);
+    }
+
+
+    public function getLanguage()
+    {
+        return $this->getParameter('lang');
+    }
+
+
+    public function setPayMethod($value)
+    {
+        return $this->setParameter('payMethod', $value);
+    }
+
+
+    public function getPayMethod()
+    {
+        return $this->getParameter('payMethod');
     }
 
 
